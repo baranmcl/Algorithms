372e0dea9c0615e8d0156b6cc6088adab7ed60e2
baranmcl <baranmcl@gmail.com>
2015-07-18 21:06:29 -0400

    added comments

--- MergeSort/MergeSort.py
+++ MergeSort/MergeSort.py
@@ -4,12 +4,13 @@ def mergeSort(listofnumbers):
     lefthalf = listofnumbers[:mid]
     righthalf = listofnumbers[mid:]
     
-    mergeSort(lefthalf)
+    mergeSort(lefthalf) #recursively moves towards the beginning of a list until it hits a base case
     mergeSort(righthalf)
     
-    i=0
-    j=0
-    k=0
+    i=0 #left index counter
+    j=0 #right index counter
+    k=0 #main list index counter
+    #this part actually does the merging
     while i<len(lefthalf) and j <len(righthalf):
       if lefthalf[i]<righthalf[j]:
         listofnumbers[k]=lefthalf[i]
