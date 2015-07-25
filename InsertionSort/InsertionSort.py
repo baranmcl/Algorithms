8f837daa1e9c27d4e1553cec5ff2251fafb0ec8a
baranmcl <baranmcl@gmail.com>
2015-07-25 10:40:55 -0400

    added algorithm
    special thanks to interactivepython.org

--- InsertionSort/InsertionSort.py
+++ InsertionSort/InsertionSort.py
@@ -1,5 +1,13 @@
 #InsertionSort
 #Iterate through each item in the list, inserting each item into the ordered list
 
-def insertionSort(SomeList):
-  
+def insertionSort(somelist):
+  for index in range(1, len(somelist)):
+    currentvalue = somelist[index]
+    position = index
+    
+    while position > 0 and somelist[position-1]>currentvalue:
+      somlist[position] = somelist[position-1]
+      position = position-1
+      
+    somelist[position] = currentvalue
